ce2370d2438571f98ac90e3fd2a1769db23b281c
Alessandro Benedetti <benedetti.alex85@gmail.com>
2017-12-02 13:02:46 +0000

    [LUCENE-7802] More Like This refactor + additional tests

---
+++ lucene/queries/src/test/org/apache/lucene/queries/mlt/terms/InterestingTermsRetrieverTestBase.java
@@ -0,0 +1,209 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.lucene.queries.mlt.terms;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queries.mlt.MoreLikeThisParameters;
+import org.apache.lucene.queries.mlt.MoreLikeThisTestBase;
+import org.apache.lucene.queries.mlt.terms.scorer.ScoredTerm;
+import org.apache.lucene.util.PriorityQueue;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
+public abstract class InterestingTermsRetrieverTestBase extends MoreLikeThisTestBase {
+  private IndexedDocumentTermsRetriever toTest;
+
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+  }
+
+  @Test
+  public void multiFieldDoc_KQueryTerms_shouldReturnTopKTerms() throws Exception {
+    //More Like This parameters definition
+    int topK = 26;
+    MoreLikeThisParameters params = getDefaultParams();
+    params.setMaxQueryTerms(topK);
+    params.setFieldNames(new String[]{FIELD1, FIELD2});
+    //Test preparation
+    PriorityQueue<ScoredTerm> scoredTerms = retrieveScoredTerms(params);
+
+    assertEquals("Expected " + topK + " terms only!", topK, scoredTerms.size());
+    //Expected terms preparation
+    int perFieldTermsSize = (topK / params.getFieldNames().length);
+    Term[] expectedField1Terms = new Term[perFieldTermsSize];
+    int idx = 0;
+    for (String text : getArithmeticSeriesWithSuffix(numDocs - perFieldTermsSize + 1, perFieldTermsSize, "a")) {
+      expectedField1Terms[idx++] = new Term(FIELD1, text);
+    }
+    Term[] expectedField2Terms = new Term[perFieldTermsSize];
+    idx = 0;
+    for (String text : getArithmeticSeriesWithSuffix(numDocs - perFieldTermsSize + 1, perFieldTermsSize, "b")) {
+      expectedField2Terms[idx++] = new Term(FIELD2, text);
+    }
+    //Expected terms assertions
+    assertScoredTermsPriorityOrder(scoredTerms, expectedField1Terms, expectedField2Terms);
+  }
+
+  @Test
+  public void multiFieldDoc_minTermFreq_shouldIgnoreTermsLessFrequent() throws Exception {
+    //More Like This parameters definition
+    int minTermFreq = 95;
+    MoreLikeThisParameters params = getDefaultParams();
+    params.setFieldNames(new String[]{FIELD1, FIELD2});
+    params.setMinTermFreq(minTermFreq);
+    //Test preparation
+    int perFieldExpectedScoredTermsSize = numDocs - minTermFreq + 1;
+    int expectedScoredTermsSize = params.getFieldNames().length * perFieldExpectedScoredTermsSize;
+
+    PriorityQueue<ScoredTerm> scoredTerms = retrieveScoredTerms(params);
+
+    assertEquals("Expected " + expectedScoredTermsSize + " terms only!", expectedScoredTermsSize, scoredTerms.size());
+    //Expected terms preparation
+    Term[] expectedField1Terms = new Term[perFieldExpectedScoredTermsSize];
+    int idx = 0;
+    for (String text : getArithmeticSeriesWithSuffix(minTermFreq, perFieldExpectedScoredTermsSize, "a")) {
+      expectedField1Terms[idx++] = new Term(FIELD1, text);
+    }
+    Term[] expectedField2Terms = new Term[perFieldExpectedScoredTermsSize];
+    idx = 0;
+    for (String text : getArithmeticSeriesWithSuffix(minTermFreq, perFieldExpectedScoredTermsSize, "b")) {
+      expectedField2Terms[idx++] = new Term(FIELD2, text);
+    }
+    //Expected terms assertions
+    assertScoredTermsPriorityOrder(scoredTerms, expectedField1Terms, expectedField2Terms);
+  }
+
+  @Test
+  public void multiFieldDoc_minDocFreq_shouldIgnoreTermsLessFrequent() throws Exception {
+    //More Like This parameters definition
+    int minDocFreq = 96;
+    MoreLikeThisParameters params = getDefaultParams();
+    params.setMinDocFreq(minDocFreq);
+    params.setFieldNames(new String[]{FIELD1, FIELD2});
+    //Test preparation
+    int expectedScoredTermsSize = 10;
+    PriorityQueue<ScoredTerm> scoredTerms = retrieveScoredTerms(params);
+
+    assertEquals("Expected " + expectedScoredTermsSize + " terms only!", expectedScoredTermsSize, scoredTerms.size());
+    //Expected terms preparation
+    int perFieldTermsSize = (expectedScoredTermsSize / params.getFieldNames().length);
+    Term[] expectedField1Terms = new Term[perFieldTermsSize];
+    int idx = 0;
+    for (String text : getArithmeticSeriesWithSuffix(1, perFieldTermsSize, "a")) {
+      expectedField1Terms[idx++] = new Term(FIELD1, text);
+    }
+    Term[] expectedField2Terms = new Term[perFieldTermsSize];
+    idx = 0;
+    for (String text : getArithmeticSeriesWithSuffix(1, perFieldTermsSize, "b")) {
+      expectedField2Terms[idx++] = new Term(FIELD2, text);
+    }
+    //Expected terms assertions
+    assertScoredTermsPriorityOrder(scoredTerms, expectedField1Terms, expectedField2Terms);
+  }
+
+  @Test
+  public void multiFieldDoc_maxDocFreq_shouldIgnoreTermsTooFrequent() throws Exception {
+    //More Like This parameters definition
+    int maxDocFreq = 5;
+    MoreLikeThisParameters params = getDefaultParams();
+    params.setMaxDocFreq(maxDocFreq);
+    params.setFieldNames(new String[]{FIELD1, FIELD2});
+    //Test preparation
+    int expectedScoredTermsSize = params.getFieldNames().length * maxDocFreq;
+    PriorityQueue<ScoredTerm> scoredTerms = retrieveScoredTerms(params);
+
+    assertEquals("Expected " + expectedScoredTermsSize + " terms only!", expectedScoredTermsSize, scoredTerms.size());
+    //Expected terms preparation
+    int perFieldTermsSize = (expectedScoredTermsSize / params.getFieldNames().length);
+    Term[] expectedField1Terms = new Term[perFieldTermsSize];
+    int idx = 0;
+    for (String text : getArithmeticSeriesWithSuffix(96, perFieldTermsSize, "a")) {
+      expectedField1Terms[idx++] = new Term(FIELD1, text);
+    }
+    Term[] expectedField2Terms = new Term[perFieldTermsSize];
+    idx = 0;
+    for (String text : getArithmeticSeriesWithSuffix(96, perFieldTermsSize, "b")) {
+      expectedField2Terms[idx++] = new Term(FIELD2, text);
+    }
+    //Expected terms assertions
+    assertScoredTermsPriorityOrder(scoredTerms, expectedField1Terms, expectedField2Terms);
+  }
+
+  @Test
+  public void multiFieldDoc_onlyField1Configured_shouldConsiderOnlyTermsFromField1() throws Exception {
+    //More Like This parameters definition
+    int topK = 26;
+    MoreLikeThisParameters params = getDefaultParams();
+    params.setMaxQueryTerms(topK);
+    params.setFieldNames(new String[]{FIELD1});
+    Map<String, Float> testBoostFactor = new HashMap<>();
+    testBoostFactor.put(FIELD2, 2.0f);
+    params.setFieldToQueryTimeBoostFactor(testBoostFactor);
+    //Test preparation
+    PriorityQueue<ScoredTerm> scoredTerms = retrieveScoredTerms(params);
+
+    assertEquals("Expected " + topK + " terms only!", topK, scoredTerms.size());
+    int countField2 = 0;
+    for (ScoredTerm term : scoredTerms) {
+      if (term.field.equals(FIELD2)) {
+        countField2++;
+      }
+    }
+
+    assertThat(countField2, is(0));
+  }
+
+  @Test
+  public void multiFieldDoc_field2Boosted_shouldConsiderMoreTermsFromField1() throws Exception {
+    //More Like This parameters definition
+    int topK = 26;
+    MoreLikeThisParameters params = getDefaultParams();
+    params.setMaxQueryTerms(topK);
+    params.setFieldNames(new String[]{FIELD1, FIELD2});
+    Map<String, Float> testBoostFactor = new HashMap<>();
+    testBoostFactor.put(FIELD2, 2.0f);
+    params.setFieldToQueryTimeBoostFactor(testBoostFactor);
+    //Test preparation
+    PriorityQueue<ScoredTerm> scoredTerms = retrieveScoredTerms(params);
+
+    assertEquals("Expected " + topK + " terms only!", topK, scoredTerms.size());
+    int countField1 = 0;
+    int countField2 = 0;
+    for (ScoredTerm term : scoredTerms) {
+      if (term.field.equals(FIELD1)) {
+        countField1++;
+      } else if (term.field.equals(FIELD2)) {
+        countField2++;
+      }
+    }
+
+    assertThat(countField1 < countField2, is(true));
+  }
+
+  protected abstract PriorityQueue<ScoredTerm> retrieveScoredTerms(MoreLikeThisParameters params) throws IOException ;
+
+
+}
+
+
